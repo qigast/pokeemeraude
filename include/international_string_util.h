9e8052e5923f82c8ff9b0b329f9215c52f6c49de
qigast <qigast@proton.me>
2025-07-05 14:37:05 +0300

    Add new function declarations in i18n utilities.

--- include/international_string_util.h
+++ include/international_string_util.h
@@ -20,5 +20,7 @@ void ConvertInternationalPlayerNameStripChar(u8 *str, u8 removeChar);
 void ConvertInternationalContestantName(u8 *str);
 int GetNicknameLanguage(u8 *str);
 void FillWindowTilesByRow(int windowId, int columnStart, int rowStart, int numFillTiles, int numRows);
+u8 *StringAppendWithPlaceholder(u8 *dest, const u8 *src, u8 *placeholderStr);
+const u8 *GetTrainerClassNameGenderSpecific(s32 trainerClassId, u32 trainerGender, const u8 *trainerName);
 
 #endif // GUARD_INTERNATIONAL_STRING_UTIL_H
